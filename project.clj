8c7534c1b091305efc8b77f4ae76e7fa150177bf
Weichu Liu <typhoonsword@gmail.com>
2017-04-25 10:28:29 +0900

    A random dependency bump

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url ""
   :license {:name "Beerware"
             :url "https://fedoraproject.org/wiki/Licensing/Beerware"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/java.jmx "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/java.jmx "0.3.4"]]
   :main ^:skip-aot jmx-dump.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
